e4c9bc2d0263501a35ca98f0fe42c362e4f0d677
Liviu Sultan <liviu.sultan@gmail.com>
2024-12-26 12:24:03 +0200

    initial commit of symfony project with docker and add migrations for creating district table with data

---
+++ migrations/Version20241226001623.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241226001623 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE district (
+        id INT AUTO_INCREMENT NOT NULL, 
+        code VARCHAR(2) NOT NULL, 
+        name VARCHAR(255) NOT NULL, 
+        UNIQUE INDEX unique_code (code), 
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE district');
+    }
+}
